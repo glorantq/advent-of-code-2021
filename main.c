010b329d967a29dd1e7addc5c6593917d7fef570
Gerber Lóránt Viktor <glorantq@gmail.com>
2021-12-12 02:55:34 +0100

    include timings in release mode

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "log.h"
 #include "solutions/days_def.h"
@@ -23,12 +24,66 @@ int main(int argc, char** argv) {
     };
 
     int methods_size = sizeof(methods) / sizeof(void (*)(execution_info_t*));
+
+#ifdef NDEBUG
+    double time_sum = 0.;
+#endif
+
+    double* method_times = malloc(sizeof(double) * methods_size);
+    memset(method_times, 0, sizeof(double) * methods_size);
+
     for (int i = 0; i < methods_size; i++) {
+        clock_t start_time;
+        clock_t end_time;
+
+        start_time = clock();
         methods[i](execution_info);
+        end_time = clock();
+
+        double time_diff = (double) (end_time - start_time) / CLOCKS_PER_SEC * 1000;
+        *(method_times + i) = time_diff;
+#ifdef NDEBUG
+        time_sum += time_diff;
+#endif
     }
 
     SUCCESS("AdventOfCode-C", "🎄 Complete");
 
+#ifdef NDEBUG
+    printf("\n");
+    double time_max = -1;
+    int max_idx = 0;
+    double time_min = 99;
+    int min_idx = 0;
+    SUCCESS("AdventOfCode-C", "==================================");
+    SUCCESS("AdventOfCode-C", "Day timings: ");
+
+    for(int i = 0; i < methods_size; i++) {
+        double time = *(method_times + i);
+        if (time < time_min) {
+            time_min = time;
+            min_idx = i;
+        }
+
+        if (time > time_max) {
+            time_max = time;
+            max_idx = i;
+        }
+    }
+
+    for(int i = 0; i < methods_size; i++) {
+        double time = *(method_times + i);
+
+        SUCCESS("AdventOfCode-C", "    Day %d: %s%.3fms", i + 1, time >= time_max ? "\033[31m" : (time >= (time_max - time_min) / 2. ? "\033[33m" : "\033[32m"), time);
+    }
+
+    SUCCESS("AdventOfCode-C", "Total time used: %.3fms", time_sum);
+    SUCCESS("AdventOfCode-C", "Average time used: %.3fms", time_sum / (double) methods_size);
+    SUCCESS("AdventOfCode-C", "Max: %.3fms (Day %d), Min: %.3fms (Day %d)", time_max, max_idx + 1, time_min, min_idx + 1);
+    SUCCESS("AdventOfCode-C", "==================================");
+#endif
+
+    free(method_times);
     free(execution_info);
     return 0;
 }
